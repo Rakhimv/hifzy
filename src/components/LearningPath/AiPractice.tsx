6c6c642c95f9e1822cdb6e734e5fcbcc8983e4e2
rakhimv <azizbekrakhimv@gmail.com>
2025-10-05 21:48:15 +0300

    anim upd

--- src/components/LearningPath/AiPractice.tsx
+++ src/components/LearningPath/AiPractice.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 const AiPractice: React.FC = () => {
 
     return (
-        <div className="w-full min-h-screen bg-white flex items-center justify-center relative z-[11]">
+        <div className="w-full min-h-screen bg-white flex flex-col items-center  relative z-[11]">
             <motion.div
-                className="relative z-[4] flex flex-col items-center"
-                initial={{ opacity: 0, y: -100 }}
+                className="flex flex-col justify-center items-center pt-[120px]"
+                initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.8, ease: [0.23, 1, 0.32, 1] }}
+                transition={{ duration: 1.2, ease: [0.23, 1, 0.32, 1] }}
             >
-                <div className="text-[72px] flex flex-col gap-[10px] text-primary max-w-[650px]">
-                    <p className="leading-[1] text-center font-semibold">
-                        Discover <span className="text-op1">the</span> Quran{" "}
-                        <span className="text-op1">with a </span>Fun Approach
-                    </p>
+                <div className="mb-[20px] text-[22px]">
+                    <button className="px-[32px] border-[2px] border-[#E4E4E4] py-[20px] cursor-pointer outline-none rounded-[22px]">AI Practice</button>
                 </div>
-                <p className="text-op2 font-medium text-[24px] max-w-[534px] text-center mt-[20px] leading-[1.3]">
-                    Memorize more effectively with the help of our tools and deepen your knowledge in theory
+                <div className="text-[92px] flex flex-col gap-[10px] text-primary max-w-[900px]">
+                    <p className="leading-[1] text-center font-semibold">Feel the True Meaning of Each Page</p>
+                </div>
+
+
+
+                <p className="text-op2 font-medium text-[28px] max-w-[730px] text-center mt-[20px] leading-[1.3]">
+                    Read the Quran with a deep understanding of all the nuances
+                    of the Arabic language and consolidate it with our AI tools
                 </p>
-                <div className="flex mt-[32px] gap-[16px] text-[22px]">
-                    <div className="relative inline-block h-max rounded-[22px] overflow-hidden">
-                        <button className="relative px-[32px] py-[20px] cursor-pointer outline-none rounded-[22px] text-white bg-gradient-to-b from-[#90C6B6] to-[#7BAF9D]">
-                            <p className="z-[3]">Start using</p>
-                        </button>
-                        <div className="absolute z-2 left-1/2 -translate-x-1/2 -translate-y-2 w-[100%] h-[50px] rounded-full bg-[#A9FCE2] blur-xl opacity-[1]"></div>
-                    </div>
-                    <button className="px-[32px] border-[2px] border-[#E4E4E4] py-[20px] cursor-pointer outline-none rounded-[22px]">Read more</button>
-                </div>
             </motion.div>
+
+
+
+
+
         </div>
     );
 };
